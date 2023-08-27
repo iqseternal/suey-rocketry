274e0c9b3817022e3e6f7740f08137f2d77aba82
Suey <liniyuanqu@163.com>
2023-08-27 18:39:26 +0800

    Delete dist directory

--- dist/esm/components/MacDocker/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MacDocker } from './src/Docker.js';
-export { DockerItem as MacDockerItem } from './src/DockerItem.js';
-export { DockerControl as MacDockerControl } from './src/DockerControl.js';
-export { DockerMain as MacDockerMain } from './src/DockerMain.js';
